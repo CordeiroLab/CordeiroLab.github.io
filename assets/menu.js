41bdd34c297ee5f633e5f93998a4299ef11acea3
Tiago Cordeiro <tiago.n.cordeiro@gmail.com>
2025-04-29 15:15:19 +0100

    Update menu.js

--- assets/menu.js
+++ assets/menu.js
@@ -1,16 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
-    const sidebar = document.getElementById('menu');
+    const menu = document.getElementById('menu');
 
-    // Toggle sidebar visibility on hamburger click
+    // Toggle menu visibility on hamburger click
     hamburger.addEventListener('click', () => {
-        sidebar.classList.toggle('active');
-    });
-
-    // Close the sidebar when clicking outside of it
-    document.addEventListener('click', (event) => {
-        if (!sidebar.contains(event.target) && !hamburger.contains(event.target)) {
-            sidebar.classList.remove('active');
-        }
+        menu.classList.toggle('active'); // Add or remove 'active' class
     });
 });
